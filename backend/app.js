5594f497ad88ff0595cad69bec692900686ed805
NoobTW <noob@noob.tw>
2019-01-09 21:01:22 +0800

    feat: socket init commit

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,73 @@
-const express = require('express');
-const http = require('http');
-const socket = require('socket.io');
-const port = 8080
-const app = express();
+const app = require('http').createServer(handler)
+const io = require('socket.io')(app);
+const { MD5 } = require('crypto-js');
 
-const server = http.createServer(app)
-const io = socket(server)
+app.listen(80);
 
+function handler (req, res) {
+  res.writeHead(200);
+  res.end();
+}
 
-const roomInfo = {}; //房間使用者名稱單
+const rooms = [];
 
-io.on('connection', (client) => {
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('CREATE_REQ', (data) => { createReq(data, socket.id) });
+  socket.on('JOIN_REQ', (data) => { joinReq(data, socket.id) });
+  socket.on('GAME_START_REQ', (data) => { gameStartReq(data, socket.id) });
+});
 
-    client.on('send_message', function(data){
-        io.emit('receive_message', data);
+function sendToRoom(label, roomId, data) {
+  const room = rooms.find(x => x.rid === roomId);
+  // io.sockets.socket(clientId).emit('ROOM_STATUS', roomStatus);
+  Array.from(room.players.map(x => x.uid)).forEach((user) => {
+    io.of('/').connected[user].emit(label, data);
+  });
+}
+
+function createReq(data, id) {
+  if( !data.name ) return;
+  console.log('[CREATE_REQ]');
+  const rid = `${Math.floor(Math.random() * 10000)}`.padStart(4, '0');
+  const newRoom = {
+    createTime: new Date(),
+    started: false,
+    rid,
+    players: [
+      {
+        name: data.name,
+        uid: id,
+        admin: true,
+      },
+    ]
+  };
+  rooms.push(newRoom);
+  const roomStatus = {
+    rid,
+    players: newRoom.players
+  };
+  sendToRoom('ROOM_STATUS', rid, roomStatus);
+};
+
+function joinReq(data, id) {
+  if (!data.name) return;
+  console.log('[JOIN_REQ]');
+  const room = rooms.find(x => x.rid === data.rid);
+  if(room){
+    if (room.players.find(x => x.uid === id)) return;
+    room.players.push({
+      name: data.name,
+      uid: id
     });
-})
+    const roomStatus = {
+      rid: data.rid,
+      players: room.players
+    };
+    sendToRoom('ROOM_STATUS', data.rid, roomStatus);
+  }
+}
 
-server.listen(port, ()=> console.log(`Listening on port ${port}`))
+function gameStartReq(data, id) {
+  
+}
