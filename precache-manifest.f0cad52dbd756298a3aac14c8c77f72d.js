afcd2d3b331dd144cb1d3aba51e867d438cf7db0
danny <tocyen@gmail.com>
2019-01-11 08:10:51 +0800

    Updates

---
+++ precache-manifest.f0cad52dbd756298a3aac14c8c77f72d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "863c6dc49a1181a6cb6bd5d8e218a68f",
+    "url": "/static/media/交配.863c6dc4.png"
+  },
+  {
+    "revision": "5b29fd09381dee7c97e1",
+    "url": "/static/css/main.42ddd358.chunk.css"
+  },
+  {
+    "revision": "17fc98d1e926625617935baf57b4849f",
+    "url": "/static/media/選擇行動背景圖.17fc98d1.png"
+  },
+  {
+    "revision": "115c229bff6d73cabd27",
+    "url": "/static/js/1.115c229b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bf2cab6167a40bfb2d9a56d196152dc",
+    "url": "/static/media/飼料.9bf2cab6.png"
+  },
+  {
+    "revision": "20a4d13cb34a230669e342a19f397f94",
+    "url": "/static/media/小公雞.20a4d13c.png"
+  },
+  {
+    "revision": "fc33caa98b881b4c6710d1dd9c9e7be9",
+    "url": "/static/media/中母雞.fc33caa9.png"
+  },
+  {
+    "revision": "c093e890351bc6012620002614bd8d8d",
+    "url": "/static/media/大公雞.c093e890.png"
+  },
+  {
+    "revision": "24cff795197ac696e1e4d3868dcac79e",
+    "url": "/static/media/疫苗.24cff795.png"
+  },
+  {
+    "revision": "502ae071194c34dfe80eef800d6b46ab",
+    "url": "/static/media/牛刀.502ae071.png"
+  },
+  {
+    "revision": "cb8dd73433c402c5e1286bb56d3d70bd",
+    "url": "/static/media/錢.cb8dd734.png"
+  },
+  {
+    "revision": "0d9245a73d9724bba40745fe51386670",
+    "url": "/static/media/炸彈.0d9245a7.png"
+  },
+  {
+    "revision": "5b29fd09381dee7c97e1",
+    "url": "/static/js/main.5b29fd09.chunk.js"
+  },
+  {
+    "revision": "e4ca2355e9645f2fd84e2d775ec4f07c",
+    "url": "/static/media/購買.e4ca2355.png"
+  },
+  {
+    "revision": "a04eaa4f6ff5255ad1adf5947c5a2225",
+    "url": "/static/media/販賣.a04eaa4f.png"
+  },
+  {
+    "revision": "4f20277c61a5f7e64ae3364cdf228b62",
+    "url": "/static/media/春天.4f20277c.png"
+  },
+  {
+    "revision": "45a889a073b6d9818a28b8ce14743f46",
+    "url": "/static/media/NPC.45a889a0.png"
+  },
+  {
+    "revision": "20a4d13cb34a230669e342a19f397f94",
+    "url": "/static/media/小母雞.20a4d13c.png"
+  },
+  {
+    "revision": "db54e4969b984ac433e87fb7914123e9",
+    "url": "/static/media/中公雞.db54e496.png"
+  },
+  {
+    "revision": "e07562b22a15434f1b9e9f949c283aae",
+    "url": "/static/media/大母雞.e07562b2.png"
+  },
+  {
+    "revision": "042351c8cff21a1e00ed972ce7558483",
+    "url": "/static/media/夏天.042351c8.png"
+  },
+  {
+    "revision": "e7f34644f72e6c2d1a9c3ab275e48fda",
+    "url": "/static/media/秋天.e7f34644.png"
+  },
+  {
+    "revision": "0341e6acbafdcb2fa7ecae35079d23c7",
+    "url": "/static/media/冬天.0341e6ac.png"
+  },
+  {
+    "revision": "1766c6f72f38d76af5537c0bb09a6ca6",
+    "url": "/static/media/雞蛋籃.1766c6f7.png"
+  },
+  {
+    "revision": "115c229bff6d73cabd27",
+    "url": "/static/css/1.d5108df9.chunk.css"
+  },
+  {
+    "revision": "f7e2110ea69f79fd7f70df17b205eca4",
+    "url": "/index.html"
+  }
+];
